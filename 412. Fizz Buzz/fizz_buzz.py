25791d7e3f3b2c5584da8a149cbb78e50d1ed201
EgorBodrov <bodroff.alexeevich@yandex.ru>
2022-04-21 18:31:45 +0300

    Add Fizz Buzz problem solution

---
+++ 412. Fizz Buzz/fizz_buzz.py
@@ -0,0 +1,14 @@
+class Solution:
+    def fizzBuzz(self, n: int) -> List[str]:
+        answer = [str(x) for x in range(1, n+1)]
+        for i in range(1, n+1):
+            tmp = ''
+            if i % 3 == 0:
+                tmp += 'Fizz'
+            if i % 5 == 0:
+                tmp += 'Buzz'
+            answer[i - 1] = tmp if tmp != '' else answer[i - 1]
+        
+        return answer
+    
+    
