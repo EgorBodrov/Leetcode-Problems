2398ca2accd03259ddceb392493f2e3583f5210c
EgorBodrov <bodroff.alexeevich@yandex.ru>
2022-04-21 13:03:33 +0300

    Improve Ransom Note problem solution

--- 383. Ransom Note/ransom_note.py
+++ 383. Ransom Note/ransom_note.py
@@ -1,16 +1,7 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        info = {}
-        for letter in magazine:
-            if letter in info:
-                info[letter] += 1
-            else:
-                info[letter] = 1
-        
-        for letter in ransomNote:
-            if letter in info and info[letter] > 0:
-                info[letter] -= 1
-            else: 
+        for letter in set(ransomNote):
+            if magazine.count(letter) < ransomNote.count(letter):
                 return False
-        
+            
         return True
